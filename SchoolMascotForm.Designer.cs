ae85e2b84561b859c7231d3a8ec22c42909f82d5
JadonF <jadon.fournier@ocsbstudent.ca>
2018-02-16 10:32:16 -0500

    Initial commit

---
+++ SchoolMascotForm.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace SchoolMascot_Jadon_F_
+{
+    partial class frmSchoolMascot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mnuSchools = new System.Windows.Forms.MenuStrip();
+            this.mniSchools = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniImmaculata = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniStJoes = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniMotherTeresa = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniStMarks = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblSchool = new System.Windows.Forms.Label();
+            this.lblMascot = new System.Windows.Forms.Label();
+            this.mnuSchools.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnuSchools
+            // 
+            this.mnuSchools.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniSchools});
+            this.mnuSchools.Location = new System.Drawing.Point(0, 0);
+            this.mnuSchools.Name = "mnuSchools";
+            this.mnuSchools.Size = new System.Drawing.Size(284, 24);
+            this.mnuSchools.TabIndex = 0;
+            this.mnuSchools.Text = "Schools";
+            // 
+            // mniSchools
+            // 
+            this.mniSchools.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniImmaculata,
+            this.mniStJoes,
+            this.mniMotherTeresa,
+            this.mniStMarks,
+            this.mniExit});
+            this.mniSchools.Name = "mniSchools";
+            this.mniSchools.Size = new System.Drawing.Size(60, 20);
+            this.mniSchools.Text = "Schools";
+            this.mniSchools.Click += new System.EventHandler(this.mniSchools_Click);
+            // 
+            // mniImmaculata
+            // 
+            this.mniImmaculata.Name = "mniImmaculata";
+            this.mniImmaculata.Size = new System.Drawing.Size(152, 22);
+            this.mniImmaculata.Text = "Immaculata";
+            this.mniImmaculata.Click += new System.EventHandler(this.mniImmaculata_Click);
+            // 
+            // mniStJoes
+            // 
+            this.mniStJoes.Name = "mniStJoes";
+            this.mniStJoes.Size = new System.Drawing.Size(152, 22);
+            this.mniStJoes.Text = "St. Joes";
+            this.mniStJoes.Click += new System.EventHandler(this.mniStJoes_Click);
+            // 
+            // mniMotherTeresa
+            // 
+            this.mniMotherTeresa.Name = "mniMotherTeresa";
+            this.mniMotherTeresa.Size = new System.Drawing.Size(152, 22);
+            this.mniMotherTeresa.Text = "Mother Teresa";
+            this.mniMotherTeresa.Click += new System.EventHandler(this.mniMotherTeresa_Click);
+            // 
+            // mniStMarks
+            // 
+            this.mniStMarks.Name = "mniStMarks";
+            this.mniStMarks.Size = new System.Drawing.Size(152, 22);
+            this.mniStMarks.Text = "St. Marks";
+            this.mniStMarks.Click += new System.EventHandler(this.mniStMarks_Click);
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(152, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // lblSchool
+            // 
+            this.lblSchool.AllowDrop = true;
+            this.lblSchool.AutoSize = true;
+            this.lblSchool.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.lblSchool.Location = new System.Drawing.Point(34, 67);
+            this.lblSchool.Name = "lblSchool";
+            this.lblSchool.Size = new System.Drawing.Size(88, 29);
+            this.lblSchool.TabIndex = 1;
+            this.lblSchool.Text = "School";
+            // 
+            // lblMascot
+            // 
+            this.lblMascot.AutoSize = true;
+            this.lblMascot.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.lblMascot.Location = new System.Drawing.Point(37, 132);
+            this.lblMascot.Name = "lblMascot";
+            this.lblMascot.Size = new System.Drawing.Size(90, 29);
+            this.lblMascot.TabIndex = 2;
+            this.lblMascot.Text = "Mascot";
+            // 
+            // frmSchoolMascot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblMascot);
+            this.Controls.Add(this.lblSchool);
+            this.Controls.Add(this.mnuSchools);
+            this.MainMenuStrip = this.mnuSchools;
+            this.Name = "frmSchoolMascot";
+            this.Text = "School Mascot";
+            this.mnuSchools.ResumeLayout(false);
+            this.mnuSchools.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnuSchools;
+        private System.Windows.Forms.ToolStripMenuItem mniSchools;
+        private System.Windows.Forms.ToolStripMenuItem mniImmaculata;
+        private System.Windows.Forms.ToolStripMenuItem mniStJoes;
+        private System.Windows.Forms.ToolStripMenuItem mniMotherTeresa;
+        private System.Windows.Forms.ToolStripMenuItem mniStMarks;
+        private System.Windows.Forms.Label lblSchool;
+        private System.Windows.Forms.Label lblMascot;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+    }
+}
+
---
+++ SchoolMascotForm.cs
@@ -0,0 +1,74 @@
+﻿/*
+ * Created by: Jadon Fournier
+ * Created on: Thursday, Feb. 2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #7 - SchoolMascot
+ * This program shows the names of schools with the name of their mascot. You can use the menu to either exit the program, or change the school shown.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace SchoolMascot_Jadon_F_
+{
+    public partial class frmSchoolMascot : Form
+    {
+        public frmSchoolMascot()
+        {
+            InitializeComponent();
+        }
+
+        private void mniSchools_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mniImmaculata_Click(object sender, EventArgs e)
+        {
+
+            lblSchool.Text = "Immaculata";
+            lblMascot.Text = "Saints";
+        }
+
+        private void mniStJoes_Click(object sender, EventArgs e)
+        {
+            lblSchool.Text = "Saint Joe's";
+            lblMascot.Text = "Coffee Cups";
+        }
+
+        private void mniMotherTeresa_Click(object sender, EventArgs e)
+        {
+            lblSchool.Text = "Mother Teresa";
+            lblMascot.Text = "Alligators";
+        }
+
+        private void mniStMarks_Click(object sender, EventArgs e)
+        {
+            lblSchool.Text = "Saint Mark's";
+            lblMascot.Text = "Matresses";
+        }
+
+        private void lblSchool_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblMascot_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
